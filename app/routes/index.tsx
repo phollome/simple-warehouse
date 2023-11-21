dac5ccad6f381f34711792fe171b01dfd7d428ee
Peter Holló <hello@songsforthe.dev>
2023-11-21 16:07:44 +0100

    implement adding items

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,4 +1,35 @@
-import type { MetaFunction } from "@remix-run/node";
+import { conform, useForm } from "@conform-to/react";
+import { parse } from "@conform-to/zod";
+import {
+  json,
+  type DataFunctionArgs,
+  type MetaFunction,
+} from "@remix-run/node";
+import { Form, useLoaderData } from "@remix-run/react";
+import { z } from "zod";
+
+const addItemsSchema = z.object({
+  name: z.string().min(3, "Must be at least 3 characters"),
+});
+
+const items: z.infer<typeof addItemsSchema>[] = [];
+
+export async function loader(args: DataFunctionArgs) {
+  return json({ items });
+}
+
+export async function action(args: DataFunctionArgs) {
+  const { request } = args;
+  const formData = await request.formData();
+  const submission = parse(formData, { schema: addItemsSchema });
+
+  if (typeof submission.value !== "undefined" && submission.value !== null) {
+    items.push(submission.value);
+    return json({ status: "success", submission });
+  }
+
+  return json({ status: "error", submission });
+}
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,33 +39,29 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const loaderData = useLoaderData<typeof loader>();
+  const [form, fields] = useForm({
+    id: "create-items-form",
+    onValidate: (args) => {
+      return parse(args.formData, { schema: addItemsSchema });
+    },
+    shouldValidate: "onSubmit",
+    shouldRevalidate: "onInput",
+  });
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
+      <h1>Items</h1>
+      <Form method="post" {...form.props}>
+        <label htmlFor={fields.name.id}>Name</label>
+        <input {...conform.input(fields.name)} />
+        {fields.name.error && <p>{fields.name.error}</p>}
+        <button type="submit">Add</button>
+      </Form>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {loaderData.items.map((item) => (
+          <li key={item.name}>{item.name}</li>
+        ))}
       </ul>
     </div>
   );
