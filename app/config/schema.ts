30d4139bd7f9f75731bf834122e07fdd35c99204
Peter Holló <hello@songsforthe.dev>
2023-12-06 16:55:29 +0100

    don't generate env names dynamically

--- app/config/schema.ts
+++ app/config/schema.ts
@@ -1,37 +1,43 @@
+import constantcase from "@stdlib/string-constantcase";
 import packageJSON from "../../package.json" assert { type: "json" };
-import { enhanceSchemaWithEnvNames } from "./utils";
 
-const schemaWithoutEnvNames = {
+const schema = {
   nodeENV: {
     doc: "The application environment.",
     format: ["development", "test", "production"],
     default: "development",
+    env: constantcase("nodeENV"),
   },
   app: {
     baseURL: {
       doc: "The base url of the application.",
       format: "url",
       default: "http://localhost:3000",
+      env: constantcase("app.baseURL"),
     },
     name: {
       doc: "The application name.",
       format: String,
       default: "New Remix App",
+      env: constantcase("app.name"),
     },
     description: {
       doc: "The application description.",
       format: String,
       default: "Welcome to Remix!",
+      env: constantcase("app.description"),
     },
     version: {
       doc: "The application version.",
       format: String,
       default: packageJSON.version,
+      env: constantcase("app.version")
     },
     numberOfItemsPerPage: {
       doc: "The number of items to display per page.",
       format: Number,
       default: 10,
+      env: constantcase("app.numberOfItemsPerPage")
     },
   },
   testing: {
@@ -40,6 +46,7 @@ const schemaWithoutEnvNames = {
         doc: "The number of milliseconds to wait after submitting a form.",
         format: Number,
         default: 100,
+        env: constantcase("testing.e2e.waitAfterSubmit")
       },
     },
   },
@@ -48,17 +55,17 @@ const schemaWithoutEnvNames = {
       doc: "The database connection URL.",
       format: String,
       default: "file:./db.sqlite",
+      env: constantcase("database.url")
     },
     seed: {
       numberOfItems: {
         doc: "The number of items to seed the database with.",
         format: Number,
         default: 10,
+        env: constantcase("database.seed.numberOfItems")
       },
     },
   },
 };
-
-const schema = enhanceSchemaWithEnvNames(schemaWithoutEnvNames);
 
 export default schema;
